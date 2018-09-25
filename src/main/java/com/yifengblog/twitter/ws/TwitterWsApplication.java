11f540ab9d01ec5acbcd5e1a49f0f4b669645444
unknown <meixx115@d.umn.edu>
2018-09-25 16:48:40 -0500

    generate war file

--- src/main/java/com/yifengblog/twitter/ws/TwitterWsApplication.java
+++ src/main/java/com/yifengblog/twitter/ws/TwitterWsApplication.java
@@ -3,13 +3,20 @@ package com.yifengblog.twitter.ws;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class TwitterWsApplication {
+public class TwitterWsApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(TwitterWsApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+	    return app.sources(TwitterWsApplication.class);
 	}
 	
 	@Bean
